0c4615aacab4dd1a0a96034928299343a7529073
Nihal Singh <nihal.111@gmail.com>
2016-05-02 20:17:57 +0530

    Added snackbar and created download button

--- app/src/main/java/com/moodld/moodld/MainActivity.java
+++ app/src/main/java/com/moodld/moodld/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Environment;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -39,42 +40,57 @@ import java.util.jar.Manifest;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button logout;
+    private Button logout, dld, preferences;
     private static final String TAG = "MainActivity";
     private String sessionCookie = null;
     private static final String mainPageUrl = "http://moodle.iitb.ac.in/";
     private ArrayList<String> downloadLinks = new ArrayList<String>();
     private ArrayList<String> courseNames = new ArrayList<String>();
-    private final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 0;
+    private final int WRITE_EXTERNAL_STORAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getWritePermission();
-
-        SharedPreferences preferences = getSharedPreferences("LoginDetails", MODE_PRIVATE);
-        String sessionCookie = preferences.getString("MoodleSession", null);
+        SharedPreferences prefs = getSharedPreferences("LoginDetails", MODE_PRIVATE);
+        String sessionCookie = prefs.getString("MoodleSession", null);
         if (sessionCookie != null) {
             JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
             jsoupAsyncTask.execute(mainPageUrl, sessionCookie);
         } else {
             Log.d(TAG, "Session cookie not present");
-            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-            startActivity(intent);
+            logout();
         }
 
         logout = (Button) findViewById(R.id.logout);
+        dld = (Button) findViewById(R.id.dld);
+        preferences = (Button) findViewById(R.id.preferences);
+
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SharedPreferences.Editor editor = getSharedPreferences("LoginDetails", MODE_PRIVATE).edit();
-                editor.putString("username", null);
-                editor.putString("password", null);
-                editor.putString("MoodleSession", null);
-                editor.apply();
+                logout();
+            }
+        });
+
+        dld.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    AskWritePermission();
+                } else {
+                    dldfiles();
+                }
+            }
+        });
+
+        preferences.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
                 Log.d(TAG, "Logged out.");
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
@@ -83,13 +99,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+    }
+
+    //onCLick of "Logout" button
+    private void logout() {
+        SharedPreferences.Editor editor = getSharedPreferences("LoginDetails", MODE_PRIVATE).edit();
+        editor.putString("username", null);
+        editor.putString("password", null);
+        editor.putString("MoodleSession", null);
+        editor.apply();
+        Log.d(TAG, "Logged out.");
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    //onClick of "DLD Files" button
+    private void dldfiles() {
+        Log.d(TAG, "DLD Files");
         String file_url = "http://moodle.iitb.ac.in/pluginfile.php/53165/mod_resource/content/0/PH_108_Kumar_ppt.pdf";
         String address = Environment.getExternalStorageDirectory().toString() + "/nihal";
-
-        final SharedPreferences prefs = MainActivity.this.getSharedPreferences("LoginDetails", MODE_PRIVATE);
-        sessionCookie = prefs.getString("MoodleSession", null);
-        Log.d(TAG, "MoodleSession=" + sessionCookie);
-
         //Download call
         new DownloadFileFromURL().execute(file_url, address);
     }
@@ -159,30 +189,70 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void getWritePermission() {
+    public void AskWritePermission(){
+        // Requesting Storage Permission
         if (ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) {
-            Log.d(TAG, "Write Storage permission not given.");
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                    MY_PERMISSIONS_REQUEST_READ_CONTACTS);
-        }
-        else {
-            Log.d(TAG, "Write storage permission given.");
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                Snackbar.make(this.getWindow().getDecorView().findViewById(android.R.id.content), "Storage access is required to download files to your device.",
+                        Snackbar.LENGTH_INDEFINITE).setAction("OK", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // Request the permission
+                        ActivityCompat.requestPermissions(MainActivity.this,
+                                new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                                WRITE_EXTERNAL_STORAGE);
+                    }
+                }).show();
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        WRITE_EXTERNAL_STORAGE);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
         }
     }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_READ_CONTACTS:
-                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Log.d(TAG, "Write Storage Permission Granted.");
+            case WRITE_EXTERNAL_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // task you need to do.
+                    dldfiles();
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Snackbar.make(this.getWindow().getDecorView().findViewById(android.R.id.content), "Allow storage access to proceed with downloading files.",
+                            Snackbar.LENGTH_INDEFINITE).setAction("OK", new View.OnClickListener() {public void onClick(View view) {}})
+                            .show();
                 }
-                else {
-                    Log.d(TAG, "Write Storage Permission denied.");
-                    Toast.makeText(MainActivity.this, "You need to allow this permission to download files!", Toast.LENGTH_SHORT).show();
-                    getWritePermission();
-                }
+
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
         }
     }
 
