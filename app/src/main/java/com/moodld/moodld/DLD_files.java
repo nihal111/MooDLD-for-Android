deac8426f9ef2405b7b070aadc7c77c63f15efdc
Arpan Banerjee <arpanbnrj9@gmail.com>
2016-07-07 22:34:59 +0530

    Calling DownloadFileFromURL in DLD_Files

--- app/src/main/java/com/moodld/moodld/DLD_files.java
+++ app/src/main/java/com/moodld/moodld/DLD_files.java
@@ -38,6 +38,7 @@ public class DLD_files extends AppCompatActivity {
     private static final String TAG = "DLD_files";
     private final int REQUEST_DIRECTORY = 0;
     private static final String mainPageUrl = "http://moodle.iitb.ac.in/";
+    private final String rootDir = "MooDLD";
     private String sessionCookie;
     ArrayList<Course> CourseList = new ArrayList<Course>();
     SharedPreferences coursePrefs;
@@ -82,14 +83,15 @@ public class DLD_files extends AppCompatActivity {
             Course course = CourseList.get(i);
             Log.d(TAG, course.getName() + ": " + course.getUrl());
             JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
-            jsoupAsyncTask.execute(course.getUrl(), sessionCookie);
+            jsoupAsyncTask.execute(course, sessionCookie);
             Log.d(TAG, course.getName());
         }
     }
 
 
-    private class JsoupAsyncTask extends AsyncTask<String, String, Void> {
+    private class JsoupAsyncTask extends AsyncTask<Object, String, Void> {
 
+        Course course;
         Elements links;
         Document htmlDocument;
 
@@ -99,13 +101,12 @@ public class DLD_files extends AppCompatActivity {
         }
 
         @Override
-        protected Void doInBackground(String... params) {
+        protected Void doInBackground(Object... params) {
             try {
-                htmlDocument = Jsoup.connect(params[0]).cookie("MoodleSession", params[1]).get();
+                course = (Course)params[0];
+                htmlDocument = Jsoup.connect(course.getUrl()).cookie("MoodleSession", (String)params[1]).get();
 //                htmlDocument = Jsoup.parse("<div class=\"summary\"></div><ul class=\"section img-text\"><li class=\"activity assign modtype_assign \" id=\"module-21233\"><div><div class=\"mod-indent-outer\"><div class=\"mod-indent\"></div><div><div class=\"activityinstance\"><a class=\"\" onclick=\"\" href=\"http://moodle.iitb.ac.in/mod/assign/view.php?id=21233\"><img src=\"http://moodle.iitb.ac.in/theme/image.php/clean/assign/1444075825/icon\" class=\"iconlarge activityicon\" alt=\" \" role=\"presentation\" /><span class=\"instancename\">Assignment 6 Submission Link: Due April 4, 2355 hours.</span></a></div></div></div></div></li><li class=\"activity resource modtype_resource \" id=\"module-22063\"><div><div class=\"mod-indent-outer\"><div class=\"mod-indent\"></div><div><div class=\"activityinstance\"><a class=\"\" onclick=\"\" href=\"http://moodle.iitb.ac.in/mod/resource/view.php?id=22063\"><img src=\"http://moodle.iitb.ac.in/theme/image.php/clean/core/1444075825/f/pdf-24\" class=\"iconlarge activityicon\" alt=\" \" role=\"presentation\" /><span class=\"instancename\">Assignment 4 Solutions<span class=\"accesshide \" > File</span></span></a></div></div></div></div></li></ul></div></li><li id=\"section-15\" class=\"section main clearfix\" role=\"region\" aria-label=\"11 April - 17 April\"><div class=\"left side\"><img width=\"1\" height=\"1\" class=\"spacer\" alt=\"\" title=\"\" src=\"http://moodle.iitb.ac.in/theme/image.php/clean/core/1444075825/spacer\" /></div><div class=\"right side\"><img width=\"1\" height=\"1\" class=\"spacer\" alt=\"\" title=\"\" src=\"http://moodle.iitb.ac.in/theme/image.php/clean/core/1444075825/spacer\" /></div><div class=\"content\"><h3 class=\"sectionname\">11 April - 17 April</h3><div class=\"summary\"></div><ul class=\"section img-text\"></ul></div></li><li id=\"section-16\" class=\"section main clearfix\" role=\"region\" aria-label=\"18 April - 24 April\"><div class=\"left side\"><img width=\"1\" height=\"1\" class=\"spacer\" alt=\"\" title=\"\" src=\"http://moodle.iitb.ac.in/theme/image.php/clean/core/1444075825/spacer\" /></div><div class=\"right side\"><img width=\"1\" height=\"1\" class=\"spacer\" alt=\"\" title=\"\" src=\"http://moodle.iitb.ac.in/theme/image.php/clean/core/1444075825/spacer\" /></div><div class=\"content\"><h3 class=\"sectionname\">18 April - 24 April</h3><div class=\"summary\"></div><ul class=\"section img-text\"><li class=\"activity resource modtype_resource \" id=\"module-22586\"><div><div class=\"mod-indent-outer\"><div class=\"mod-indent\"></div><div><div class=\"activityinstance\"><a class=\"\" onclick=\"\" href=\"http://moodle.iitb.ac.in/mod/resource/view.php?id=22586\"><img src=\"http://moodle.iitb.ac.in/theme/image.php/clean/core/1444075825/f/pdf-24\" class=\"iconlarge activityicon\" alt=\" \" role=\"presentation\" /><span class=\"instancename\">Endsem solutions.<span class=\"accesshide \" > File</span></span></a></div></div></div></div></li><li class=\"activity assign modtype_assign \" id=\"module-22589\"><div><div class=\"mod-indent-outer\"><div class=\"mod-indent\"></div><div><div class=\"activityinstance\"><a class=\"\" onclick=\"\" href=\"http://moodle.iitb.ac.in/mod/assign/view.php?id=22589\"><img src=\"http://moodle.iitb.ac.in/theme/image.php/clean/assign/1444075825/icon\" class=\"iconlarge activityicon\" alt=\" \" role=\"presentation\" /><span class=\"instancename\">Endsem <span class=\"accesshide \" > Assignment</span></span></a></div></div></div></div></li></ul></div></li><li id=\"section-17\" class=\"section main clearfix\" role=\"region\" aria-label=\"25 April - 1 May\"><div class=\"left side\"><img width=\"1\" height=\"1\" class=\"spacer\" alt=\"\" title=\"\" src=\"http://moodle.iitb.ac.in/theme/image.php/clean/core/1444075825/spacer\" /></div><div class=\"right side\"><img width=\"1\" height=\"1\" class=\"spacer\" alt=\"\" title=\"\" src=\"http://moodle.iitb.ac.in/theme/image.php/clean/core/1444075825/spacer\" /></div><div class=\"content\"><h3 class=\"sectionname\">25 April - 1 May</h3><div class=\"summary\"></div><ul class=\"section img-text\"></ul></div></li><li id=\"section-18\" class=\"section main clearfix\" role=\"region\" aria-label=\"2 May - 8 May\"><div class=\"left side\"><img width=\"1\" height=\"1\" class=\"spacer\" alt=\"\" title=\"\" src=\"http://moodle.iitb.ac.in/theme/image.php/clean/core/1444075825/spacer\" /></div><div class=\"right side\"><img width=\"1\" height=\"1\" class=\"spacer\" alt=\"\" title=\"\" src=\"http://moodle.iitb.ac.in/theme/image.php/clean/core/1444075825/spacer\" /></div><div class=\"content\"><h3 class=\"sectionname\">2 May - 8 May</h3><div class=\"summary\"></div><ul class=\"section img-text\"><li class=\"activity resource modtype_resource \" id=\"module-22857\"><div><div class=\"mod-indent-outer\"><div class=\"mod-indent\"></div><div><div class=\"activityinstance\"><a class=\"\" onclick=\"\" href=\"http://moodle.iitb.ac.in/mod/resource/view.php?id=22857\"><img src=\"http://moodle.iitb.ac.in/theme/image.php/clean/core/1444075825/f/pdf-24\" class=\"iconlarge activityicon\" alt=\" \" role=\"presentation\" /><span class=\"instancename\">Assignment 5 Solutions.<span class=\"accesshide \" > File</span></span></a></div></div></div></div></li><li class=\"activity resource modtype_resource \" id=\"module-22858\"><div><div class=\"mod-indent-outer\"><div class=\"mod-indent\"></div><div><div class=\"activityinstance\"><a class=\"\" onclick=\"\" href=\"http://moodle.iitb.ac.in/mod/resource/view.php?id=22858\"><img src=\"http://moodle.iitb.ac.in/theme/image.php/clean/core/1444075825/f/pdf-24\" class=\"iconlarge activityicon\" alt=\" \" role=\"presentation\" /><span class=\"instancename\">Assignment 6: Divider implementation.<span class=\"accesshide \" > File</span>");
                 links = htmlDocument.select("a[href]");
-                //Iterate over links and call DownloadFileFromUrl
-
 //                Log.d(TAG, links.toString());
             } catch (IOException e) {
                 e.printStackTrace();
@@ -115,9 +116,11 @@ public class DLD_files extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(Void result) {
-            Course course;
+            //Iterate over links and call DownloadFileFromUrl
             for (Element link : links) {
                 if (!link.attr("abs:href").startsWith(mainPageUrl + "logout.php") || !link.attr("abs:href").startsWith(mainPageUrl + "mod/forum") || !link.attr("abs:href").startsWith(mainPageUrl + "my") || !link.attr("abs:href").startsWith(mainPageUrl + "user") || !link.attr("abs:href").startsWith(mainPageUrl + "badges") || !link.attr("abs:href").startsWith(mainPageUrl + "my") || !link.attr("abs:href").startsWith(mainPageUrl + "user") || !link.attr("abs:href").startsWith(mainPageUrl + "calendar")|| !link.attr("abs:href").startsWith(mainPageUrl + "my") || !link.attr("abs:href").startsWith(mainPageUrl + "user") || !link.attr("abs:href").startsWith(mainPageUrl + "grade")|| !link.attr("abs:href").startsWith(mainPageUrl + "my") || !link.attr("abs:href").startsWith(mainPageUrl + "user") || !link.attr("abs:href").startsWith(mainPageUrl + "message")) {
+                    DownloadFileFromURL download = new DownloadFileFromURL();
+                    download.execute(course.getUrl(), rootDir+course.getName().substring(0,6));
                     Log.d(TAG, link.text() + ": " + link.attr("abs:href"));
                 }
             }
