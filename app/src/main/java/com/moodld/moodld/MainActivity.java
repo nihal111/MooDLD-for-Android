513e51ca6be54a150689e0d204a108cb4ad11079
Nihal Singh <nihal.111@gmail.com>
2016-07-20 02:46:29 +0530

    Added sidebar

--- app/src/main/java/com/moodld/moodld/MainActivity.java
+++ app/src/main/java/com/moodld/moodld/MainActivity.java
@@ -4,18 +4,37 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.mikepenz.fontawesome_typeface_library.FontAwesome;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
+import com.mikepenz.materialdrawer.util.AbstractDrawerImageLoader;
+import com.mikepenz.materialdrawer.util.DrawerImageLoader;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -31,10 +50,12 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+    public AccountHeader headerResult;
     private ProgressBar progressBar;
     private Button logout, dld, preferences;
     private static final String TAG = "MainActivity";
-    private String sessionCookie = null;
+    private String sessionCookie = null, email = null;
     private static final String mainPageUrl = "http://moodle.iitb.ac.in/";
     private ArrayList<String> downloadLinks = new ArrayList<String>();
     private ArrayList<String> courseNames = new ArrayList<String>();
@@ -54,6 +75,8 @@ public class MainActivity extends AppCompatActivity {
 
         SharedPreferences prefs = getSharedPreferences("LoginDetails", MODE_PRIVATE);
         sessionCookie = prefs.getString("MoodleSession", null);
+        email = prefs.getString("username", null);
+        email = email + "@iitb.ac.in";
         if (sessionCookie != null) {
             JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
             jsoupAsyncTask.execute(mainPageUrl, sessionCookie);
@@ -98,6 +121,87 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        InitialiseDrawer();
+
+    }
+
+
+    public void InitialiseDrawer() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.title_dashboard);
+        //if you want to update the items at a later time it is recommended to keep it in a variable
+        PrimaryDrawerItem preferences = new PrimaryDrawerItem().withName(R.string.nav_item_preference).withIdentifier(1).withSelectable(false).withIcon(FontAwesome.Icon.faw_wrench);
+        PrimaryDrawerItem directory = new PrimaryDrawerItem().withName(R.string.nav_item_directory).withIdentifier(2).withSelectable(false).withIcon(FontAwesome.Icon.faw_download);
+        PrimaryDrawerItem help = new PrimaryDrawerItem().withName(R.string.nav_item_help).withIdentifier(3).withSelectable(false).withIcon(FontAwesome.Icon.faw_question);
+        PrimaryDrawerItem feedback = new PrimaryDrawerItem().withName(R.string.nav_item_feedback).withIdentifier(4).withSelectable(false).withIcon(FontAwesome.Icon.faw_comment);
+        PrimaryDrawerItem about = new PrimaryDrawerItem().withName(R.string.nav_item_about).withIdentifier(5).withSelectable(false).withIcon(FontAwesome.Icon.faw_info);
+        PrimaryDrawerItem logout = new PrimaryDrawerItem().withName(R.string.nav_item_logout).withIdentifier(6).withSelectable(false).withIcon(FontAwesome.Icon.faw_sign_out);
+
+//        DrawerImageLoader.init(new AbstractDrawerImageLoader() {
+//            @Override
+//            public void set(ImageView imageView, Uri uri, Drawable placeholder) {
+//                // super.set(imageView, uri, placeholder);
+//                Log.d(TAG, "Loading Profile picture via Picasso");
+//                Picasso.with(Dashboard.this).load(uri).resize(100, 100).centerCrop().into(imageView);
+//            }
+//
+//        });
+
+        // Create the AccountHeader
+        headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.header)
+                .withSelectionListEnabledForSingleProfile(false)
+                .build();
+
+
+        //create the drawer and remember the `Drawer` result object
+        Drawer result = new DrawerBuilder()
+                .withAccountHeader(headerResult)
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .addDrawerItems(
+                        preferences, directory, new DividerDrawerItem(), help, feedback, about, new DividerDrawerItem(), logout
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        if (drawerItem != null && drawerItem.getIdentifier() == 1) {
+                        }
+                        if (drawerItem != null && drawerItem.getIdentifier() == 2) {
+                        }
+                        if (drawerItem != null && drawerItem.getIdentifier() == 3) {
+                        }
+                        if (drawerItem != null && drawerItem.getIdentifier() == 4) {
+                        }
+                        if (drawerItem != null && drawerItem.getIdentifier() == 5) {
+                        }
+                        if (drawerItem != null && drawerItem.getIdentifier() == 6) {
+                        }
+
+                        return false;
+                    }
+                })
+                .withOnDrawerListener(new Drawer.OnDrawerListener() {
+                    @Override
+                    public void onDrawerOpened(View drawerView) {
+
+                    }
+
+                    @Override
+                    public void onDrawerClosed(View drawerView) {
+
+                    }
+
+                    @Override
+                    public void onDrawerSlide(View drawerView, float slideOffset) {
+
+                    }
+                })
+                .withSelectedItem(-1)
+                .build();
 
     }
 
@@ -224,11 +328,11 @@ public class MainActivity extends AppCompatActivity {
                 if (link.attr("abs:href").startsWith(mainPageUrl + "course")) {
                     downloadLinks.add(link.attr("abs:href"));
                     courseNames.add(link.text().substring(0, 6));
-                } else if (link.attr("abs:href").startsWith(mainPageUrl + "user/profile.php")) {
+                } else if (link.attr("abs:href").startsWith(mainPageUrl + "user/profile.php") && !link.text().equals("My profile") && !link.text().equals("View profile")) {
                     String myname = link.text();
                     Log.d(TAG, myname);
-                    TextView nameTV = (TextView) findViewById(R.id.nameTextView);
-                    nameTV.setText("Welcome, " + myname);
+                    final IProfile profile = new ProfileDrawerItem().withName(myname).withEmail(email).withIcon(ResourcesCompat.getDrawable(getResources(), R.drawable.user, null));
+                    headerResult.addProfiles(profile);
                 }
             }
             Log.d(TAG, downloadLinks.toString());
